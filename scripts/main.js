b6ff4b57a57e92c815dd122d8166f9a5384d7c8a
youemomlol <7515701@isd535.org>
2021-09-09 09:37:37 -0500

    made shapes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
-// Put your JavaScript here
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF00FF";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "pink";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(190, 144, 212, 1)";
+ctx.stroke();
+ctx.closePath();
